3774c2c7819e495012dba0f3fb89281fa561bafe
Maca Sants <macarenasants@gmail.com>
2026-06-24 17:38:07 -0300

    correccion de payload en request

--- tests/api/tc04-empty-cart-api.spec.js
+++ tests/api/tc04-empty-cart-api.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { API } = require('../helpers/helpers');
 const { obtenerCookieUsuario } = require('../helpers/cookies');
+const { PRODUCTS } = require('../data/products');
 
 test('TC04 - Empty cart API', async ({ page, request }) => {
   await page.goto('/');
@@ -9,7 +10,7 @@ test('TC04 - Empty cart API', async ({ page, request }) => {
 
   const tieneCarrito = await request.post(`${API}/viewcart`, {
     data: {
-      cookie: 'cookies',
+      cookie: cookie,
     },
   });
   await expect(tieneCarrito.status()).toBe(200);
@@ -19,15 +20,26 @@ test('TC04 - Empty cart API', async ({ page, request }) => {
   expect(response.Items).toEqual([]);
 });
 
-test('TC04 - Add product to cart API, and delete it', async ({ request }) => {
+test('TC04 - Add product to cart API, and delete it', async ({ page , request }) => {
   const productoID = PRODUCTS.SONY_XPERIA_Z5.id;
-  const tieneCarrito = await cargarCarrito(request, productoID);
+   await page.goto('/');
 
-  expect(tieneCarrito.status()).toBe(200);
+  const cookie = await obtenerCookieUsuario(page);
+  
+  const addToCartResponse = await request.post(`${API}/addtocart`, {
+    data: {
+      cookie: cookie,
+      id: "8d725f3e-7651-2325-4cff-6345195cf43f", 
+      prod_id: productoID,
+      flag: false
+    },
+  });
+
+  expect(addToCartResponse.status()).toBe(200);
 
   const response = await request.post(`${API}/deletecart`, {
     data: {
-      cookie: 'cookie',
+      cookie: cookie,
     },
   });
 
