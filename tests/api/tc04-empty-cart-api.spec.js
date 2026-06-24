60a1614b4a93b45cfb8b4ccbba7f6c732a8817d7
Maca Sants <macarenasants@gmail.com>
2026-06-24 15:58:36 -0300

    actualizacion de tc4 api

--- tests/api/tc04-empty-cart-api.spec.js
+++ tests/api/tc04-empty-cart-api.spec.js
@@ -7,15 +7,32 @@ test('TC04 - Empty cart API', async ({ page, request }) => {
 
   const cookie = await obtenerCookieUsuario(page);
 
+  const tieneCarrito = await request.post(`${API}/viewcart`, {
+    data: {
+      cookie: 'cookies',
+    },
+  });
+  await expect(tieneCarrito.status()).toBe(200);
+  const response = await tieneCarrito.json();
+  console.log('Che esta vacio? ', response.Items);
+
+  expect(response.Items).toEqual([]);
+});
+
+test('TC04 - Add product to cart API, and delete it', async ({ request }) => {
+  const productoID = PRODUCTS.SONY_XPERIA_Z5.id;
+  const tieneCarrito = await cargarCarrito(request, productoID);
+
+  expect(tieneCarrito.status()).toBe(200);
+
   const response = await request.post(`${API}/deletecart`, {
     data: {
-      cookie: cookie,
+      cookie: 'cookie',
     },
   });
 
   expect(response.status()).toBe(200);
+  expect(await response.text()).toContain('Item deleted');
 
-  const body = await response.text();
-
-  expect(body).toContain('Item deleted');
+  console.log(await response.json());
 });
